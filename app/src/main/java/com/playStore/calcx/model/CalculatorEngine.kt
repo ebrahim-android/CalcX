e2039a0c058c6c58077f52cd3684a68f108fa52c
Ebrahim Santana <ebrahimsantana35@gmail.com>
2025-11-24 16:17:53 -0400

    feat(engine): add normalization and sanitization steps for scientific expressions
    refactor(engine): rewrite evaluate with safe error handling and improved result validation

--- app/src/main/java/com/playStore/calcx/model/CalculatorEngine.kt
+++ app/src/main/java/com/playStore/calcx/model/CalculatorEngine.kt
@@ -5,12 +5,58 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 class CalculatorEngine {
 
-    fun evaluate(expression: String):Double{
-        val exp = ExpressionBuilder(expression)
-            .functions(CustomFunctions.all)
-            .operator(CustomOperators.all)
-            .build()
+    fun evaluate(expression: String): Double? {
+        return try {
+            // evaluate the expression and return the result (double)
+            val normalized = normalizeFunctions(expression)
+            val sanitized = sanitize(normalized)
 
-        return exp.evaluate()
+            val exp = ExpressionBuilder(sanitized)
+                // add custom functions and operators
+                .functions(CustomFunctions.all)
+                .operator(CustomOperators.all)
+                .build()
+
+            val result = exp.evaluate()
+
+            if (result.isNaN() || result.isInfinite()) null else result
+        } catch (e: Exception) {
+            null
+        }
     }
+
+
+    private fun sanitize(expr: String): String { // turn virtual symbol into real ones to exp4j
+        return expr
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace("−", "-")
+            .replace("√", "sqrt")
+//        .replace("%", "/100")   // so far
+            .trim()
+    }
+}
+
+private fun normalizeFunctions(expr: String): String {
+    return expr
+        // powers
+        .replace("x²", "^(2)")
+        .replace("x^", "^")
+
+        // Roots
+        .replace("√x", "sqrt")
+        .replace("√", "sqrt")
+
+        // Logarithms
+        .replace("log", "log10")
+        .replace("ln", "log")
+
+        // Factorial
+        .replace("n!", "fact")
+
+        // Constants
+        .replace("π", "pi")
+
+        // absolute value
+        .replace("abs", "abs")
 }
